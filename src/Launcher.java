433365823dd8b83d8a095cb44f404eb2fe5a9967
karimsalhi <karim.salhi@epita.fr>
2022-02-09 15:28:27 +0100

    Fibo done

--- src/Launcher.java
+++ src/Launcher.java
@@ -1,6 +1,15 @@
 import java.util.Scanner;
+import static java.lang.Integer.parseInt;
 
 public class Launcher {
+    public static int fibo(int n) {
+        if (n == 0)
+            return 0;
+        if (n == 1)
+            return 1;
+        return fibo(n - 1) + fibo(n - 2);
+    }
+
     public static void main(String[] args) {
         while(true) {
             Scanner scan = new Scanner(System.in);
@@ -9,6 +18,11 @@ public class Launcher {
 
             if (res.equals("quit"))
                 break;
+            else if (res.equals("fibo")) {
+                res = scan.nextLine();
+                int resFibo = fibo(parseInt(res));
+                System.out.println(resFibo);
+            }
             else
                 System.out.println("Unknown command");
         }
