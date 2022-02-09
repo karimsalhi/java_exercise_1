ac5fee856e0248e5894815f97202a5ce4ae260a4
karimsalhi <karim.salhi@epita.fr>
2022-02-09 14:53:41 +0100

    Loop done

--- src/Launcher.java
+++ src/Launcher.java
@@ -2,13 +2,15 @@ import java.util.Scanner;
 
 public class Launcher {
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
+        while(true) {
+            Scanner scan = new Scanner(System.in);
 
-        String res = scan.nextLine();
+            String res = scan.nextLine();
 
-        if (res.equals("quit"))
-            return;
-        else
-            System.out.println("Unknown command");
+            if (res.equals("quit"))
+                break;
+            else
+                System.out.println("Unknown command");
+        }
     }
 }
