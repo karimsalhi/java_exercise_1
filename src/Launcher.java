db00169c7f8fa30b206ba4c47a6ad7dd8847e960
Karim Salhi <karim.salhi@epita.fr>
2022-02-09 18:06:44 +0100

    Freq can read files

--- src/Launcher.java
+++ src/Launcher.java
@@ -1,7 +1,19 @@
+import java.io.IOException;
+import java.nio.file.Path;
 import java.util.Scanner;
 import static java.lang.Integer.parseInt;
 
 public class Launcher {
+    public static boolean openFile(Path path) {
+        try {
+            String file = java.nio.file.Files.readString(path);
+            return true;
+        } catch (Exception e) {
+            System.out.println("Unreadable file: " + e);
+            return false;
+        }
+    }
+
     public static int fibo(int n) {
         if (n == 0)
             return 0;
@@ -23,6 +35,12 @@ public class Launcher {
                 res = scan.nextLine();
                 int resFibo = fibo(parseInt(res));
                 System.out.println(resFibo);
+            }
+            else if (res.equals("freq")) {
+                System.out.println("Entrez le chemin vers un fichier:");
+                res = scan.nextLine();
+                boolean fileOpened = openFile(Path.of(res));
+                System.out.println(fileOpened);
             }
             else
                 System.out.println("Unknown command");
